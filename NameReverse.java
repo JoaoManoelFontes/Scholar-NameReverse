a05856562e31760ae48877720e915d104f577de0
Manu <84148859+manugGates@users.noreply.github.com>
2021-05-31 09:42:54 -0300

    Create NameReverse.java

---
+++ NameReverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NameReverse {
+  public static void main (String [] args) {
+   
+   Scanner sc= new Scanner(System.in);
+  
+   System.out.println ("digite um nome");
+   String name = sc.nextLine();
+    
+    int length = name.length(); 
+      System.out.println(name+" ao contrário:");
+     for(int i=1;i<=length;i++){
+            String reverse = name.substring(length-i,length-(i-1));
+            System.out.print(reverse);
+     }
+
+
+ }
+}
