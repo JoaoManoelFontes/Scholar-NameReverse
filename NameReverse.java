d3c63a61197fbb6752b9be88fd5a8e929c10d5bf
Manu <84148859+JoaoManoelFontes@users.noreply.github.com>
2021-12-14 11:20:45 -0300

    Update NameReverse.java

--- NameReverse.java
+++ NameReverse.java
@@ -7,12 +7,14 @@ public class NameReverse {
   
    System.out.println ("digite um nome");
    String name = sc.nextLine();
+   System.out.println("");
     
-    int length = name.length(); 
-      System.out.println(name+" ao contrário:");
-     for(int i=1;i<=length;i++){
-            String reverse = name.substring(length-i,length-(i-1));
-            System.out.print(reverse);
+   int length = name.length(); 
+   System.out.println(name+" ao contrário:");
+    
+   for(int i=1;i<=length;i++){
+       String reverse = name.substring(length-i,length-(i-1));
+       System.out.print(reverse);
      }
 
 
